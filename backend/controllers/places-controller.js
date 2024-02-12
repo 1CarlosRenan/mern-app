a6ee1c1b5ac1c68dcfb6cf3168078e1d9db16f4b
1carlosrenan <carlosrenan96@gmail.com>
2024-02-12 19:16:58 -0300

    fix: update get places by user id

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -29,9 +29,9 @@ const getPlaceById = (req, res, next) => {
   res.json({ place })
 }
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find(p => {
+  const place = DUMMY_PLACES.filter(p => {
     return p.creator === userId
   })
 
@@ -82,7 +82,7 @@ const deletePlace = (req, res, next) => {
 }
 
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
