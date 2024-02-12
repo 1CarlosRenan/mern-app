aae62f5765daff9f1d8af39729fffea1c4737d06
1carlosrenan <carlosrenan96@gmail.com>
2024-02-11 22:24:44 -0300

    feat: handling errors base

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,11 @@ const app = express();
 app.use('/api/places', placesRoutes)
 
 app.use((error, req, res, next) => {
-  if(res.headerSent) {
+  if (res.headerSent) {
     return next(error)
   }
-  res.status(error.code )
+  res.status(error.code || 500)
+  res.json({ message: error.message || 'An unknown error occurred!' })
 })
 
 app.listen(5000)
